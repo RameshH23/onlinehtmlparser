cb396e650687c9f9e5b64fa9742d085afd8edffd
Ramesh <rameshh@unitedtechno.com>
2017-11-03 12:51:31 +0530

    Set Comment

--- OnlineHTMLParser/Controllers/HomeController.cs
+++ OnlineHTMLParser/Controllers/HomeController.cs
@@ -18,6 +18,8 @@ namespace OnlineHTMLParser.Controllers
 {
     public class HomeController : Controller
     {
+        /* Home Controller */
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
